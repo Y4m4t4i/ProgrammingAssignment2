c3fa696b554a46696ee67f1dbf9b6abb8376bd46
Y4m4t4i <Y4m4t4i@users.noreply.github.com>
2015-08-16 16:47:51 +0200

    Provide implementation for the requested functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation and there may be some
+## benefit to caching the inverse of a matrix rather than compute it repeatedly
+## (there are also alternatives to matrix inversion that we will not cover here).
 
-## Write a short comment describing this function
+## We assume that the matrix supplied is always invertible.
 
+## This function creates a special "matrix" object that can cache its inverse.
+##
+## The first function, makeCacheMatrix creates a special "matrix", which is
+## really a list containing a function to:
+## - set the matrix
+## - get the matrix
+## - set the matrix inverse
+## - get the matrix inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  matrixInverse <- NULL
+  set <- function(y) {
+    x <<- y
+    # Reset inverse if matrix is set, so we don't have a wrong cached value
+    matrixInverse <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(solve) matrixInverse <<- solve
+  getInverse <- function() matrixInverse
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## This function computes the inverse of the special "matrix" returned by
+## makeCacheMatrix above. If the inverse has already been calculated (and the
+## matrix has not changed), then the cachesolve should retrieve the inverse from
+## the cache.
+## Return a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  matrixInverse <- x$getInverse()
+  if(!is.null(matrixInverse)) {
+    message("getting cached data")
+    return(matrixInverse)
+  }
+  data <- x$get()
+  matrixInverse <- solve(data, ...)
+  x$setInverse(matrixInverse)
+  matrixInverse
 }
